6a982e005cd6be8c527224ec4302a572a8e6da90
Gumraze <154658933+Gumraze-git@users.noreply.github.com>
2026-01-24 23:39:33 +0900

    Title: 사칙연산 연습, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260124/사칙연산 연습/practice-basic-arithmetic-operations.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 30;
+        int c = a + b;
+        System.out.println(c/8);
+        Systme.out.println(c%8);
+    }
+}
