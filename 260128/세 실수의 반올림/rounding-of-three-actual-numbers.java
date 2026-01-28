7da6c3400c39a7da8af564946e380ad597e92939
Gumraze <154658933+Gumraze-git@users.noreply.github.com>
2026-01-28 21:41:39 +0900

    Title: 세 실수의 반올림, Time: 146ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 260128/세 실수의 반올림/rounding-of-three-actual-numbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double d1 = sc.nextDouble();
+        double d2 = sc.nextDouble();
+        double d3 = sc.nextDouble();
+
+        System.out.printf("%.3f%n", d1);
+        System.out.printf("%.3f%n", d2);
+        System.out.printf("%.3f%n", d3);
+    }
+}
