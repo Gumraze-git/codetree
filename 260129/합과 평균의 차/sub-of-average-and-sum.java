51b33ba52fe9868e80a4503e96ce679926497fd8
Gumraze <154658933+Gumraze-git@users.noreply.github.com>
2026-01-29 17:48:36 +0900

    Title: 합과 평균의 차, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

--- 260129/합과 평균의 차/sub-of-average-and-sum.java
+++ 260129/합과 평균의 차/sub-of-average-and-sum.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt()
-        int b = sc.nextInt()
-        int c = sc.nextInt()
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
 
         int sum = a + b + c;
         int mean = sum / 3;
