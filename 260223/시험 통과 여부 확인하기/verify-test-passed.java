6eb72ed2c2abee611e016bf116b70f4f7278df2c
Gumraze <154658933+Gumraze-git@users.noreply.github.com>
2026-02-23 10:48:51 +0900

    Title: 시험 통과 여부 확인하기, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260223/시험 통과 여부 확인하기/verify-test-passed.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int score = sc.nextInt();
+
+        System.out.println(
+            score >= 80
+            ? "pass"
+            : 80 - score + " more score"
+        );
+    }
+}
