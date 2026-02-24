e76ed8f36b0aa802d7b747b6d17bfbfd1a43e7db
Gumraze <154658933+Gumraze-git@users.noreply.github.com>
2026-02-24 13:34:21 +0900

    Title: N번 더하기, Time: 90ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260224/N번 더하기/add-n-times.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            a += n;
+            System.out.println(a);
+        }
+    }
+}
