a5ffec8ba22b33b6c152ca0ccb6a40be4bff6c74
Gumraze <154658933+Gumraze-git@users.noreply.github.com>
2026-02-24 13:31:11 +0900

    Title: 문자열 출력하기, Time: 90ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260224/문자열 출력하기/print-string.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int input = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < input; i++) {
+            System.out.println("LeebrosCode");
+        }
+    }
+}
